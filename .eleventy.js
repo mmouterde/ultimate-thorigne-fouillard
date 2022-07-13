d9eed7735543721703eb8df9106418fbd5a75725
mmouterde <martin.mouterde@gmail.com>
2022-07-13 16:36:18 +0200

    publish src/documents content

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/*.webmanifest");
 	eleventyConfig.addPassthroughCopy("src/*.ico");
 	eleventyConfig.addPassthroughCopy("src/*.xml");
+	eleventyConfig.addPassthroughCopy("src/documents/*");
 	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
 		if (outputPath && outputPath.endsWith(".html")) {
 			return htmlmin.minify(content, {
