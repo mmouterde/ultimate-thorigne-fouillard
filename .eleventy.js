713051e4c08129140af67fd5f577bf4d36650286
François Freitag <mail@franek.fr>
2025-11-08 16:40:02 +0100

    Teach eleventy to compile CSS files

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { promisify } = require("node:util");
+const execFile = promisify(require("node:child_process").execFile);
 const { minify } = require('html-minifier-terser');
 
 module.exports = function (eleventyConfig) {
@@ -9,6 +11,17 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/*.ico");
 	eleventyConfig.addPassthroughCopy("src/*.xml");
 	eleventyConfig.addPassthroughCopy("src/documents/*");
+	eleventyConfig.addTemplateFormats("css");
+	eleventyConfig.addExtension(
+		"css",
+		{
+			outputFileExtension: "css",
+			compile: async function(inputContent, inputPath) {
+				const { stdout } = await execFile("tailwindcss", ["--input", inputPath], {timeout: 3000});
+				return async () => stdout
+			},
+		}
+	);
 	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
 		if (outputPath && outputPath.endsWith(".html")) {
 			return minify(content, {
